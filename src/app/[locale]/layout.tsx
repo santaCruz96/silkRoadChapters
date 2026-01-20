b29bbc5ef1986329040c1c864950e0be306c9cf3
Vyacheslav Ardalin <slavik@Slavik.localdomain>
2026-01-20 17:28:24 +0300

    переподключение локалей

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
-import {NextIntlClientProvider} from 'next-intl';
-import {getMessages, getLocale} from 'next-intl/server';
+import {NextIntlClientProvider, hasLocale} from 'next-intl';
+import {notFound} from 'next/navigation';
+import {routing} from '@/i18n/routing';
 import { Inter } from "next/font/google";
 import Header from "@/layouts/Header";
 import SearchOverlay from "@/components/modules/Search/SearchOverlay";
@@ -19,20 +20,27 @@ export const metadata: Metadata = {
   description: "",
 };
 
+type Props = {
+  children: React.ReactNode;
+  params: Promise<{locale: string}>;
+};
+
 export default async function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  const locale = await getLocale();
-  const messages = await getMessages();
+  params
+}: Props) {
+
+  const {locale} = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
 
   return (
     <html lang={locale}>
       <body
         className={`${inter.variable} antialiased`}
       >
-        <NextIntlClientProvider messages={messages}>
+        <NextIntlClientProvider>
           <Header />
           <SearchOverlay/>
           <Modal/>
