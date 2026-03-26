deb7e5eb5ca6acdcf0add30771ba120fa2a173c4
Vyacheslav Ardalin <slavik@Slavik.localdomain>
2026-03-26 12:32:04 +0300

    opengraph image fix 2

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -34,27 +34,8 @@ export async function generateMetadata(
   const t = await getTranslations({ locale, namespace: "Hero.meta" });
 
   return {
-    metadataBase: new URL("https://dev.silkroadchapters.uz"),
     title: t("title"),
     description: t("description"),
-    openGraph: {
-      title: t("title"),
-      description: t("description"),
-      images: [
-        {
-          url: "/images/og-image.png",
-          width: 1200,
-          height: 630,
-          alt: t("title"),
-        },
-      ],
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: t("title"),
-      description: t("description"),
-      images: ["/images/og-image.png"],
-    },
   };
 }
 
