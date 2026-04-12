cc955176aa83882ffd462135d315914e9e1591f3
Vyacheslav Ardalin <slavik@Slavik.localdomain>
2026-04-12 13:05:25 +0300

    content page images fix

--- src/components/common/ContentCards.tsx
+++ src/components/common/ContentCards.tsx
@@ -1,5 +1,6 @@
 import { ContentCardsProps } from "@/types/props/ContentCards.props";
 import { useFullscreenImage } from "@/store/useFullscreenImageStore";
+import Image from 'next/image';
 
 export default function ContentCards({numberOfCards, images, image}: ContentCardsProps) {
 
@@ -10,22 +11,24 @@ export default function ContentCards({numberOfCards, images, image}: ContentCard
     if (image && numberOfCards === 1) {
         return (
             <div 
-                className="rounded-[20px] sm:rounded-[30px] w-full h-46.25 sm:h-126.25 
-                    bg-cover bg-center bg-no-repeat cursor-pointer"
-                style={{ backgroundImage: `url(${image})`}}
+                className="relative rounded-[20px] sm:rounded-[30px] overflow-hidden
+                    w-full h-46.25 sm:h-126.25 cursor-pointer"
                 onClick={() => open(image)}
-            />
+            >
+                <Image src={image} alt="" fill className="object-cover" />
+            </div>
         )
     }
 
     if (numberOfCards === 1) {
         return (
             <div 
-                className="rounded-[20px] sm:rounded-[30px] w-full h-46.25 sm:h-126.25 
-                    bg-cover bg-center bg-no-repeat cursor-pointer"
-                style={{ backgroundImage: `url(${images[0].imageUrl})`}}
+                className="relative rounded-[20px] sm:rounded-[30px] overflow-hidden
+                    w-full h-46.25 sm:h-126.25 cursor-pointer"
                 onClick={() => open(images[0].imageUrl)}
-            />
+            >
+                <Image src={images[0].imageUrl} alt="" fill className="object-cover" />
+            </div>
         )
     }
 
@@ -33,17 +36,19 @@ export default function ContentCards({numberOfCards, images, image}: ContentCard
         return (
             <div className="flex gap-4">
                 <div 
-                    className="rounded-[20px] sm:rounded-[30px] w-full h-46.25 sm:h-126.25  
-                        bg-cover bg-center bg-no-repeat cursor-pointer"
-                    style={{ backgroundImage: `url(${images[0].imageUrl})`}}
+                    className="relative rounded-[20px] sm:rounded-[30px] overflow-hidden
+                        w-full h-46.25 sm:h-126.25 cursor-pointer"
                     onClick={() => open(images[0].imageUrl)}
-                />
+                >
+                    <Image src={images[0].imageUrl} alt="" fill className="object-cover" />
+                </div>
                 <div 
-                    className="rounded-[20px] sm:rounded-[30px] w-full h-46.25 sm:h-126.25  
-                        bg-cover bg-center bg-no-repeat cursor-pointer"
-                    style={{ backgroundImage: `url(${images[1].imageUrl})`}}
+                    className="relative rounded-[20px] sm:rounded-[30px] overflow-hidden
+                        w-full h-46.25 sm:h-126.25 cursor-pointer"
                     onClick={() => open(images[1].imageUrl)}
-                />
+                >
+                    <Image src={images[1].imageUrl} alt="" fill className="object-cover" />
+                </div>
             </div>
         )
     }
@@ -51,26 +56,30 @@ export default function ContentCards({numberOfCards, images, image}: ContentCard
     if (numberOfCards === 4) {
         return (
             <div className="grid grid-cols-3 gap-4 h-55 sm:h-148">
-                <div className="col-span-2 rounded-xl sm:rounded-[30px]
-                    bg-cover bg-center bg-no-repeat cursor-pointer"
-                    style={{ backgroundImage: `url(${images[0].imageUrl})`}}
+                <div className="relative col-span-2 rounded-xl sm:rounded-[30px]
+                        overflow-hidden cursor-pointer"
                     onClick={() => open(images[0].imageUrl)}
-                />
-                <div className="col-span-1 rounded-xl sm:rounded-[30px]
-                    bg-cover bg-center bg-no-repeat cursor-pointer"
-                    style={{ backgroundImage: `url(${images[1].imageUrl})`}}
+                >
+                    <Image src={images[0].imageUrl} alt="" fill className="object-cover" />
+                </div>
+                <div className="relative col-span-1 rounded-xl sm:rounded-[30px]
+                        overflow-hidden cursor-pointer"
                     onClick={() => open(images[1].imageUrl)}
-                />
-                <div className="col-span-1 rounded-xl sm:rounded-[30px]
-                    bg-cover bg-center bg-no-repeat cursor-pointer"
-                    style={{ backgroundImage: `url(${images[2].imageUrl})`}}
+                >
+                    <Image src={images[1].imageUrl} alt="" fill className="object-cover" />
+                </div>
+                <div className="relative col-span-1 rounded-xl sm:rounded-[30px]
+                        overflow-hidden cursor-pointer"
                     onClick={() => open(images[2].imageUrl)}
-                />
-                <div className="col-span-2 rounded-xl sm:rounded-[30px]
-                    bg-cover bg-center bg-no-repeat cursor-pointer"
-                    style={{ backgroundImage: `url(${images[3].imageUrl})`}}
+                >
+                    <Image src={images[2].imageUrl} alt="" fill className="object-cover" />
+                </div>
+                <div className="relative col-span-2 rounded-xl sm:rounded-[30px]
+                        overflow-hidden cursor-pointer"
                     onClick={() => open(images[3].imageUrl)}
-                />
+                >
+                    <Image src={images[3].imageUrl} alt="" fill className="object-cover" />
+                </div>
             </div>
         )
     }
@@ -78,31 +87,36 @@ export default function ContentCards({numberOfCards, images, image}: ContentCard
     if (numberOfCards === 5) {
         return (
             <div className="grid grid-cols-6 gap-4 h-55 sm:h-148">
-                <div className="col-span-2 rounded-xl sm:rounded-[30px]
-                    bg-cover bg-center bg-no-repeat cursor-pointer"
-                    style={{ backgroundImage: `url(${images[0].imageUrl})`}}
+                <div className="relative col-span-2 rounded-xl sm:rounded-[30px]
+                        overflow-hidden cursor-pointer"
                     onClick={() => open(images[0].imageUrl)}
-                />
-                <div className="col-span-2 rounded-xl sm:rounded-[30px]
-                    bg-cover bg-center bg-no-repeat cursor-pointer"
-                    style={{ backgroundImage: `url(${images[1].imageUrl})`}}
+                >
+                    <Image src={images[0].imageUrl} alt="" fill className="object-cover" />
+                </div>
+                <div className="relative col-span-2 rounded-xl sm:rounded-[30px]
+                        overflow-hidden cursor-pointer"
                     onClick={() => open(images[1].imageUrl)}
-                />
-                <div className="col-span-2 rounded-xl sm:rounded-[30px]
-                    bg-cover bg-center bg-no-repeat cursor-pointer"
-                    style={{ backgroundImage: `url(${images[2].imageUrl})`}}
+                >
+                    <Image src={images[1].imageUrl} alt="" fill className="object-cover" />
+                </div>
+                <div className="relative col-span-2 rounded-xl sm:rounded-[30px]
+                        overflow-hidden cursor-pointer"
                     onClick={() => open(images[2].imageUrl)}
-                />
-                <div className="col-span-3 rounded-xl sm:rounded-[30px]
-                    bg-cover bg-center bg-no-repeat cursor-pointer"
-                    style={{ backgroundImage: `url(${images[3].imageUrl})`}}
+                >
+                    <Image src={images[2].imageUrl} alt="" fill className="object-cover" />
+                </div>
+                <div className="relative col-span-3 rounded-xl sm:rounded-[30px]
+                        overflow-hidden cursor-pointer"
                     onClick={() => open(images[3].imageUrl)}
-                />
-                <div className="col-span-3 rounded-xl sm:rounded-[30px]
-                    bg-cover bg-center bg-no-repeat cursor-pointer"
-                    style={{ backgroundImage: `url(${images[4].imageUrl})`}}
+                >
+                    <Image src={images[3].imageUrl} alt="" fill className="object-cover" />
+                </div>
+                <div className="relative col-span-3 rounded-xl sm:rounded-[30px]
+                        overflow-hidden cursor-pointer"
                     onClick={() => open(images[4].imageUrl)}
-                />
+                >
+                    <Image src={images[4].imageUrl} alt="" fill className="object-cover" />
+                </div>
             </div>
         )
     }
